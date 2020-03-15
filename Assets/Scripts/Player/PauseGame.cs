df0505caebd0f83dc2f6b8ecd695f2c78d43cb9f
FelipeDermann <felipedermann@hotmail.com>
2020-03-14 21:52:13 -0300

    Pause and spell fix

---
+++ Assets/Scripts/Player/PauseGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool paused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            if (!paused)
+            {
+                paused = true;
+                Pause();
+            }
+            else
+            {
+                paused = false;
+                Unpause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    private void Unpause()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
